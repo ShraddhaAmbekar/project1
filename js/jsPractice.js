7d7a3b42a3ed2923b2c942e45ed9035461e65b47
ShraddhaAmbekar <ambekarshraddha21@gmail.com>
2023-10-15 22:01:52 +0530

    js practice

---
+++ js/jsPractice.js
@@ -0,0 +1,114 @@
+let fName= 'Harry';
+console.log(fName);
+
+var firstName="Shraddha";
+console.log(firstName);
+var last;
+console.log(last);
+console.log(last)
+console.log(fName)
+var gmail="ambekarldkjflafkahfdd"
+console.log(gmail);
+let x, y, z;  // Statement 1
+x = 5;        // Statement 2
+y = 6;        // Statement 3
+z = x + y;    // Statement 4
+console.log(z);
+let a, b, c;  // Declare 3 variables
+a = 5;        // Assign the value 5 to a
+b = 6;        // Assign the value 6 to b
+c = a + b;  
+console.log(c);
+// How to create variables:
+var p;
+let q;
+
+// How to use variables:
+p = 20;
+q = 6;
+let r = p + q;
+console.log(r);
+
+z=(5 + 6) * 10;
+console.log(z);
+z=5 * 10;
+console.log(z);
+let pq=x*100;
+console.log(pq);
+p='john';
+q='doe';
+console.log(p+" "+q);
+var d, e;
+d = 5 + 6;
+e = x * 10;
+console.log(d+e);
+let u='3';
+
+let f='3'
+k=3
+console.log(u,f,k);
+let areYouFine=true;
+console.log(areYouFine);
+
+
+let yz=300;
+c='hii';
+console.log(c);
+let v='hello';
+console.log(v);
+const l='kavya';
+console.log(l);
+const lp='kavya';
+console.log(lp);
+let person = "John Doe",
+carName = "Volvo",
+price = 200;
+console.log(person+"="+carName+'='+price);
+let city = "pune"; //string data type
+
+console.log('city 1',city);
+
+city = 'mumbai';
+console.log('city2',city);
+var o=3;
+var o=5;
+console.log(o);
+{var o=2;};
+console.log(o);
+{let o=2;}
+console.log(o);
+let j = 10;
+// Here j is 10
+
+{
+let j = 2;
+// Here j is 2
+}
+const cars = ["Saab", "Volvo", "BMW"];//Saab changed with Toyota
+console.log(cars);
+
+cars[0] = "Toyota";
+console.log(0);
+const i = 10;
+// Here i is 10
+
+{
+const i = 2;
+// Here i is 2
+}
+console.log(i);
+
+
+caName = "Volvo";
+var caName;
+console.log(caName);
+
+ (carName);
+const arName = "Volvo";
+console.log(arName);
+{let x = 2;
+let y = 2;
+let z = x ** y;//exponential
+console.log(z);}
+typeof(z);
+console.log(typeof(z));
