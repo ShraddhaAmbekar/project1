1288ff269656a5993655e56c25920ea9f4668fae
ShraddhaAmbekar <ambekarshraddha21@gmail.com>
2023-10-22 09:07:34 +0530

    conditional statement

---
+++ js/conditional.js
@@ -0,0 +1,34 @@
+let m=35;
+// if else
+if (m>=90) {
+console.log("Distingtion")   ; 
+}
+else if (m>=75){
+console.log("Grade 'A'")
+}
+else if (m>=60){
+    console.log("Grade 'B'")
+}
+else if (m>=50){
+    console.log("Grade 'c'")
+}
+
+else if (m>=36){
+    console.log("pass")
+}
+else{
+    console.log("pass")
+
+// switch
+}
+switch (runs=30) {
+    case 50:
+        console.log("half sentury");
+        break;
+    case 100:
+        console.log("sentury");
+        break;
+    default:
+        console.log("all the best");
+        break;
+}
