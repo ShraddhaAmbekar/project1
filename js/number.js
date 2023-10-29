b1db51b483dff6c95d34b59af3f5ea23c81fc365
ShraddhaAmbekar <ambekarshraddha21@gmail.com>
2023-10-29 09:08:16 +0530

    number Method practice

---
+++ js/number.js
@@ -0,0 +1,42 @@
+
+
+
+let Number = 549.384
+let number2= Number.toString()
+console.log(number2);
+// console.log(typeOf(number2));
+//console.log(number2.typeOf());
+
+console.log(Number.toExponential());
+console.log(Number.toFixed());
+
+let x= 0.2
+y = 0.1
+z=x+y
+console.log(z)
+
+let bin= 101000110001001001001;
+console.log(bin);
+
+a= new Date("2023-09-01")
+console.log(a);
+n="3456454"
+m=parseInt(n)
+console.log(a);
+c = parseInt(a)
+console.log(c + n);
+console.log(n);
+console.log(typeof(c+"<br>"+m));
+console.log(typeof(m));
+console.log(typeof(c));
+
+console.log(typeof(c+m));
+pa=parseFloat("10.3");
+console.log(pa);
+pb=parseInt("10.6");
+console.log(pb);
+pc=parseFloat("10");
+console.log(pc);
+// console.log(Number.isInteger(pa));
+//console.log(Number.isInteger(10));
+
--- js/string.js
+++ js/string.js
@@ -1,3 +1,53 @@
+let string = "Hello Shraddha";
+let s = string.length;
 
+console.log(s);
  
+console.log(string.slice(6));
+console.log(string.slice(0,5));
+console.log(string.substring(0,5));
+console.log(string.substr(0,5));
+let string2=string.replace("Hello","Namaste")
+console.log(string2);
+
+let string3="I live in Kolhapur,I love Kolhapur,and you must see all Kolhapur"
+console.log(string3.replaceAll("Kolhapur","Pune"));
+console.log(string3.replaceAll("kolhapur","Pune"));
+console.log(string3.replace(/kolhapur/i,"Pune"));
+console.log(string3.replace(/kolhapur/ig,"Pune"));
+console.log(string3.replaceAll("Kolhapur","Pune"));
+console.log(string.toUpperCase());
+console.log(string.toLowerCase());
+console.log(string.concat(" "+string2));
+
+let string4="   Hello   Hii    ";
+console.log(string4.trim());
+console.log(string4.trimEnd());
+console.log(string4.trimStart());
+text='13';
+console.log(text.padStart(15,'ak'));
+console.log(text.padEnd(15,'ak'));
+console.log(string.charAt(4));
+console.log(string.charCodeAt(4));
+console.log(string.split(""));
+console.log(string3.indexOf("love"));
+console.log(string3.indexOf("love",7));
+let index = string3.search(/you/);
+console.log(index);
+console.log(string3.indexOf("you"));
+
+let string5 = "I love cats. Cats are very easy to love. Cats are very popular."
+//let iterator = string5.match("Cats");
+console.log(string5.match("Cats"));
+console.log(string5.matchAll("Cats"));
+console.log(string5.includes("love"));
+console.log(string5.startsWith("love"))
+console.log(string5.endsWith("popular."))
+
+console.log(`${string4}, ${string5}`);
+// console.log(/\s+/g)
+// string
+console.log(string5.substring(4,8));
+
+
 
