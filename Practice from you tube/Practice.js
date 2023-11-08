d0b62fdd27fa8fad747befcb15f9924d52d1c213
ShraddhaAmbekar <ambekarshraddha21@gmail.com>
2023-11-08 22:15:01 +0530

    Practice

---
+++ Practice from you tube/Practice.js
@@ -0,0 +1,21 @@
+//alert('Good Morning!')
+//alert('Shraddha')
+console.log(10 + 5)
+
+let h1 = `I do
+practice
+very well`
+console.log(h1);
+console.log("I do practice very well");
+
+let e = 40;
+let f = "50";
+g=`40`
+console.log(">", e> f); //false it will check only for values
+console.log("<", e < f); //true
+console.log(e==g)
+
+let x = 20;
+let y = 30;
+let res = x + y;
+console.log(res);
